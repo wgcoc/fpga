fcd6bfd95a9d7ac7d962a24291264dcc83d1dd3c
wgco <1542997453@qq.com>
2018-10-25 17:17:51 +0800

    *

--- AX4010/demos/led_test/src/led_test.v
+++ AX4010/demos/led_test/src/led_test.v
@@ -1,66 +1,43 @@
-//////////////////////////////////////////////////////////////////////////////////
-//  led test                                                                    //
-//                                                                              //
-//  Author: meisq                                                               //
-//          msq@qq.com                                                          //
-//          ALINX(shanghai) Technology Co.,Ltd                                  //
-//          heijin                                                              //
-//     WEB: http://www.alinx.cn/                                                //
-//     BBS: http://www.heijin.org/                                              //
-//                                                                              //
-//////////////////////////////////////////////////////////////////////////////////
-//                                                                              //
-// Copyright (c) 2017,ALINX(shanghai) Technology Co.,Ltd                        //
-//                    All rights reserved                                       //
-//                                                                              //
-// This source file may be used and distributed without restriction provided    //
-// that this copyright statement is not removed from the file and that any      //
-// derivative work contains the original copyright notice and the associated    //
-// disclaimer.                                                                  //
-//                                                                              //
-//////////////////////////////////////////////////////////////////////////////////
-
-//================================================================================
-//  Revision History:
-//  Date          By            Revision    Change Description
-//--------------------------------------------------------------------------------
-//  2017/7/19     meisq          1.0         Original
-//*******************************************************************************/
-
-`timescale 1ns / 1ps
-module led_test
-(
-	input           clk,           // system clock 50Mhz on board
-	input           rst_n,         // reset ,low active
-	output reg[3:0] led            // LED,use for control the LED signal on board
-);
-
-//define the time counter
-reg [31:0]      timer;
-
-// cycle counter:from 0 to 4 sec
-always@(posedge clk or negedge rst_n)
-begin
-	if (rst_n == 1'b0)
-		timer <= 32'd0;                     //when the reset signal valid,time counter clearing
-	else if (timer == 32'd199_999_999)      //4 seconds count(50M*4-1=199999999)
-		timer <= 32'd0;                     //count done,clearing the time counter
-	else
-		timer <= timer + 32'd1;             //timer counter = timer counter + 1
-end
-
-// LED control
-always@(posedge clk or negedge rst_n)
-begin
-	if (rst_n == 1'b0)
-		led <= 4'b0000;                     //when the reset signal active
-	else if (timer == 32'd49_999_999)       //time counter count to 1st sec,LED1 lighten
-		led <= 4'b0001;
-	else if (timer == 32'd99_999_999)       //time counter count to 2nd sec,LED2 lighten
-		led <= 4'b0010;
-	else if (timer == 32'd149_999_999)      //time counter count to 3rd sec,LED3 lighten
-		led <= 4'b0100;
-	else if (timer == 32'd199_999_999)      //time counter count to 4th sec,LED4 lighten
-		led <= 4'b1000;
-end
-endmodule
+module led_test(clk,rst,led_out);
+	input clk,rst;
+	output led_out; //与LED引脚相连
+	
+	reg led_reg;	 //过程中使用
+	reg [31:0] timer; //32位的计时器
+	
+	/*主频50MHz，1个clk是20ns，那么1s（1000000000ns）就需要
+	需要1000000000ns/20ns=50000000个clk。又由于timer从0开始
+	计数，所以0~49999999个clk就是1s。
+	*/
+	parameter time_1s = 32'd49_999_999;
+	
+/*	
+	//initial通常用于仿真模块中对激励矢量的描述，或对寄存器变量赋初值，
+	//而在实际电路中，赋初值是没有意义的。因此这是面向模拟仿真的过程语句，
+	//通常不能被综合工具所接受，或在综合时被忽略。
+   initial
+	begin
+		led_reg = 1'b0;
+		timer = 32'b0;
+	end
+*/
+	
+	//此模块并不参与LED闪烁的直接实现
+	always@(posedge clk or negedge rst)
+	begin
+		if(rst == 0) //复位
+			begin 
+				led_reg <= 1'b0;
+				timer <= 32'b0;
+			end
+		else if(timer == time_1s)
+			begin
+				led_reg <= ~led_reg;
+				timer <= 32'b0;
+			end
+		else
+			timer <= timer + 1'b1;
+	end
+	
+	assign led_out = led_reg;
+endmodule 
